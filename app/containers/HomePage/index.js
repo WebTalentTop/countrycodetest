5f7eed11c6d1880f15cc2f8cae6eedf62a56e40b
mario <toptal.alltime@gmail.com>
2017-11-01 15:14:30 -0600

    Home index

---
+++ app/containers/HomePage/index.js
@@ -0,0 +1,105 @@
+/*
+ * HomePage
+ *
+ * This is the first thing users see of our App, at the '/' route
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
+
+import injectReducer from 'utils/injectReducer';
+import injectSaga from 'utils/injectSaga';
+import { makeSelectRepos, makeSelectLoading, makeSelectError } from 'containers/App/selectors';
+import H1 from 'components/H1';
+import ReposList from 'components/ReposList';
+import AtPrefix from './AtPrefix';
+import Form from './Form';
+import Input from './Input';
+import Section from './Section';
+import messages from './messages';
+import { loadRepos } from '../App/actions';
+import { changeCountrycode } from './actions';
+import { makeSelectCountrycode } from './selectors';
+import reducer from './reducer';
+import saga from './saga';
+
+export class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  /**
+   * when initial state countrycode is not null, submit the form to load repos
+   */
+  componentDidMount() {
+    if (this.props.countrycode && this.props.countrycode.trim().length > 0) {
+      this.props.onSubmitForm();
+    }
+  }
+
+  render() {
+    const { loading, error, repos } = this.props;
+    const reposListProps = {
+      loading,
+      error,
+      repos,
+    };
+
+    return (
+      <article>
+        <div>
+          <Section>
+            <H1>
+              Enter Country Code
+            </H1>
+            
+            <ReposList {...reposListProps} />
+          </Section>
+        </div>
+      </article>
+    );
+  }
+}
+
+HomePage.propTypes = {
+  loading: PropTypes.bool,
+  error: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.bool,
+  ]),
+  repos: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.bool,
+  ]),
+  onSubmitForm: PropTypes.func,
+  countrycode: PropTypes.string,
+  onChangeCountrycode: PropTypes.func,
+};
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    onChangeCountrycode: (evt) => dispatch(changeCountrycode(evt.target.value)),
+    onSubmitForm: (evt) => {
+      if (evt !== undefined && evt.preventDefault) evt.preventDefault();
+      dispatch(loadRepos());
+    },
+  };
+}
+
+const mapStateToProps = createStructuredSelector({
+  repos: makeSelectRepos(),
+  countrycode: makeSelectCountrycode(),
+  loading: makeSelectLoading(),
+  error: makeSelectError(),
+});
+
+const withConnect = connect(mapStateToProps, mapDispatchToProps);
+
+const withReducer = injectReducer({ key: 'home', reducer });
+const withSaga = injectSaga({ key: 'home', saga });
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(HomePage);
